c478bb45be2d97ce986bd9ef1ae37810319bc1cb
devlev1980 <devlev1980@gmail.com>
2023-12-21 07:40:08 +0200

    [FEAT] add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {LottieComponent} from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LottieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'lottie-uploader';
+  lottieData: any[] = [];
+  url: string[] = [];
+  files: any[] = [];
+  names: string[] = [];
+
+  onFileSelected(event: any) {
+    this.files = event.target.files;
+    for (let i = 0; i < this.files.length; i++) {
+      const file = this.files[i];
+      this.names.push(file.name);
+     this.url.push(URL.createObjectURL(file));
+      // const reader = new FileReader();
+      // reader.onload = (e: any) => {
+      //   this.lottieData.push(JSON.parse(e.target.result));
+      // };
+      // reader.readAsText(file);
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {LottieModule} from 'ngx-lottie';
+export function playerFactory() {
+  return import('lottie-web');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),importProvidersFrom(LottieModule.forRoot({player: playerFactory}))]
 };
